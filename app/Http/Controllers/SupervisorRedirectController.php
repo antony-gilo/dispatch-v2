9ae38d4b98a418b9f2d25631be8cc289876839b0
Antony Gilo <giloantony@gmail.com>
2021-12-24 18:40:08 +0300

    Adding in dashboard cards, hospitals card unfinished editis

--- app/Http/Controllers/SupervisorRedirectController.php
+++ app/Http/Controllers/SupervisorRedirectController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Ambulance;
+use App\Models\Dispatch;
+use App\Models\Location;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class SupervisorRedirectController extends Controller
@@ -17,6 +21,19 @@ class SupervisorRedirectController extends Controller
     {
         $users = User::all();
         $user = Auth::user();
-        return view('supervisor.index', compact('users', 'user'));
+        $drivers = User::where('role_id', 3)->count();
+        $dispatchers = User::where('role_id', [1, 2])->count();
+
+        $ambulances = Ambulance::all();
+        $on_duty = $ambulances->where('status', 1)->count();
+        $stand_by = $ambulances->where('status', 0)->count();
+
+        $dispatches = Dispatch::all()->count();
+        $current_month = Carbon::now()->month();
+        $monthly_dispatch = Dispatch::whereMonth('created_at', '=', $current_month)->get()->count();
+
+        $locations = Location::all();
+
+        return view('supervisor.index', compact('users', 'user', 'ambulances', 'dispatches', 'monthly_dispatch', 'stand_by', 'on_duty', 'drivers', 'dispatchers', 'locations'));
     }
 }
